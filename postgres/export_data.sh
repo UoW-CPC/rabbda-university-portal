9c62f32de8f20b8ed207854e612324d08e3e9c82
Dimitris Kagialis <kagialisd@gmail.com>
2020-06-16 15:22:59 +0300

    version: 0.0

--- postgres/export_data.sh
+++
@@ -1,5 +0,0 @@
-for table in student grades_high_school grades_university;
-do pg_dump -t $table -U postgres university > /home/maria_dev/$table.sql;
-done;
-
-sudo -u postgres pg_dump -t student -U postgres university > /home/maria_dev/student.sql;
---
+++ postgres/instert_missing_data.sql
@@ -0,0 +1,4 @@
+INSERT INTO students (STUDENT_ID, NAME, CAMPUS, STUDY_FIELD, REGISTRATION_YEAR, AGE , SEX, NATIONALITY, HOME_ADDRESS, HIGH_SCHOOL, HIGH_SCHOOL_ADDRESS)
+VALUES
+  ( 1873,'Alice','Campus Region C','History',2019,19,'F','UK','Region B', 'High School 2','Glasgow, Scotland'),
+  ( 1854,'Dimitris','Campus Region A','Arts',2018,20,'M','German','Region A','High School 2','Berlin, Germany');
